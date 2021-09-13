2f0fb1c9b82707fd9826f7d1c431d617936bdd83
TheVirtualRealm <70163600+TheVirtualRealm@users.noreply.github.com>
2021-09-13 09:32:30 -0400

    Add files via upload

---
+++ Money Maker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Money_Maker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Money Maker!");
+            Console.WriteLine("Please enter the amount you would like to convert");
+            string amount = Console.ReadLine();
+            double amount2 = Convert.ToDouble(amount);
+
+            Console.WriteLine($"{amount2} is equal to:");
+
+            int goldVal = 10;
+            int silverVal = 5;
+
+            double goldCoin = Math.Floor(amount2 / goldVal);
+            double remainder = amount2 % goldVal;
+
+            Console.WriteLine(goldCoin);
+            Console.WriteLine(remainder);
+
+
+            double silverCoin = Math.Floor(remainder / silverVal);
+            Console.WriteLine(silverCoin);
+            Console.WriteLine(remainder);
+
+
+            Console.WriteLine($"Gold Coins = {goldCoin}");
+            Console.WriteLine($"Silver Coins = {silverCoin}");
+            Console.WriteLine($"Bronze Coins = {remainder}");
+
+
+        }
+    }
+}
